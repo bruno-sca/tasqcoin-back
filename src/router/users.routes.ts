9eab31966d5ff9606333cb50d2e115557687f480
Bruno Assunção <bruno.scassuncao@gmail.com>
2022-04-11 20:45:24 -0300

    feat: add user info controller

--- src/router/users.routes.ts
+++ src/router/users.routes.ts
@@ -3,13 +3,16 @@ import { Router } from 'express';
 import { ensureAuthenticated } from '@middlewares/ensureAuthenticated';
 import { ChangeUserNameController } from '@modules/users/useCases/changeUserName/ChangeUserNameController';
 import { CreateUserController } from '@modules/users/useCases/createUserUseCase/CreateUserController';
+import { GetUserInfoController } from '@modules/users/useCases/getUserInfo/GetUserInfoController';
 
 const userRoutes = Router();
 
 const createUserController = new CreateUserController();
 const changeUserNameController = new ChangeUserNameController();
+const getUserInfoController = new GetUserInfoController();
 
 userRoutes.post('/', createUserController.handle);
+userRoutes.get('/', getUserInfoController.handle);
 
 userRoutes.patch(
   '/change-name',
